db6a9c54d824e424c6233a9f295449e93c4716a0
BharathKumarReddyPutluri <77716478+BharathKumarReddyPutluri@users.noreply.github.com>
2021-01-22 14:03:17 +0530

    Add files via upload

---
+++ day_3/Find_Angle_MBC.py
@@ -0,0 +1,5 @@
+import math
+AB = int(input())
+BC = int(input())
+MBC=(round(math.degrees(math.atan(AB/BC))))
+print(str(MBC)+'°')
---
+++ day_3/Polar_Coordinates.py
@@ -0,0 +1,4 @@
+import cmath
+z = complex(input())
+out = cmath.polar(z)
+print(out[0],out[1],sep="\n")
